6ed0e05aa240a0635e254b2680fddd84ba788d77
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-01-10 17:43:29 -0300

    Maintenance

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ pyautogui.click(x=1286, y=843)
 #Step 4: Calculate the indicators (income and quantity of products)
 
 time.sleep(3)
-tabela = pandas.read_excel(r'C:\Users\andre\Downloads\Vendas - Dez.xlsx')
+table = pandas.read_excel(r'C:\Users\andre\Downloads\Vendas - Dez.xlsx')
 
-faturamento = tabela['Valor Final'].sum()
-quantidade = tabela['Quantidade'].sum()
+billing = table['Valor Final'].sum()
+amount = table['Quantidade'].sum()
 
 # Step 5: Send an e-mail to board of directors
 
@@ -47,19 +47,20 @@ pyautogui.click(x=123, y=255) # Click to write email
 pyautogui.write('andreluis0703@gmail.com')
 pyautogui.press('enter')
 pyautogui.press('tab')
-pyperclip.copy('Relatório de Vendas')
+pyperclip.copy('Sales Report')
 pyautogui.hotkey('ctrl', 'v')
 pyautogui.press('tab')
 
-texto = """
+text = f"""
 Dears, good morning!
 
-Yesterday's billing was: R${faturamento:,.2f}
-The number of products was: R${quntidade:,}
+Yesterday's billing was: R${billing:,.2f}
+The number of products was: {amount:,}
 
 SY, André Ramos
 """
 
-pyperclip.copy(texto)
+pyperclip.copy(text)
 pyautogui.hotkey('ctrl', 'v')
+time.sleep(3)
 pyautogui.hotkey('ctrl', 'enter')
