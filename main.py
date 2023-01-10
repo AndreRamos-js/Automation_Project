81365f6a4d24e5351249e0e5725eef41ced5ef97
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-01-09 23:37:20 -0300

    Entering the company's system

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pyautogui
+import pyperclip
 import pandas
 import numpy
 import openpyxl
+import time
+
+pyautogui.PAUSE = 2
 
 
+# Step 1: Enter the company's system
+
+pyautogui.hotkey('ctrl', 'alt', '0')
+pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
+time.sleep(3)
+
+# Step 2: Navigate to the report location
 
