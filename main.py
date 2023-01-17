70d67d3d4da39ab365f377b9dd0b81f50796562f
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-01-16 23:32:15 -0300

    Maintenance

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ pyautogui.PAUSE = 1
 # Step 1: Enter the company's system
 
 pyautogui.hotkey('ctrl', 'alt', '0') # Open the browser
-pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga')
+pyperclip.copy('https://drive.google.com/drive/u/5/folders/1-IO1PVmfVQkPxahLVI7vpyFeElwIgm1K')
 pyautogui.hotkey('ctrl', 'v')
 pyautogui.press('enter')
 
 # Step 2: Navigate to the report location
 
 time.sleep(3) # Waiting time in seconds
-pyautogui.click(x=519, y=416, clicks=2)
+pyautogui.click(x=1567, y=476)
 
 # Step 3: Export the report
 
 time.sleep(3)
-pyautogui.click(x=609, y=500)
 pyautogui.click(x=1609, y=236)
-pyautogui.click(x=1286, y=843)
+pyautogui.click(x=1408, y=797)
 
 #Step 4: Calculate the indicators (income and quantity of products)
 
